0f4af21072ac704b99c53f418375e049b2353abd
Sametcan Al <sametcanal53@hotmail.com>
2024-06-08 14:18:03 +0300

    SignalR instant data extraction

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.DataAccessLayer.Concretes;
 
 namespace SignalRApi.Hubs
 {
     public class SignalRHub : Hub
     {
+        SignalRContext _context = new SignalRContext();
+
+        public async Task SendCategoryCount()
+        {
+            var result = _context.Categories.Count();
+            await Clients.All.SendAsync("ReceiveCategoryCount", result);
+        }
     }
 }
